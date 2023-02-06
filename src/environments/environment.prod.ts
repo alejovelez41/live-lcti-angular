6778b467eabdad2f05f09f5c83b71fe2df57d24c
alejovelez41 <avelezv@unal.edu.co>
2023-02-06 09:52:28 +0100

    ajout endpoint render sur internet pour test accesibilité

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://livelctibackbdd.fabrique-dig.dcpro31.opk.recouv'
+  // apiEndpoint: 'http://livelctibackbdd.fabrique-dig.dcpro31.opk.recouv'
+  apiEndpoint: 'https://live-lcti.onrender.com'
 };
 
 //pour la version sur internet 
